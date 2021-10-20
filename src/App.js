695627a68df6249290f2d5a5a2c0291ec20e537d
salhindi <calisaraalhindi@gmail.com>
2021-10-20 13:07:44 -0700

    added redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/memes', {
+      method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  }
+  
+
   render() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 import App from './App';
 
+let store = createStore(applyMiddleware(thunk))
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
   document.getElementById('root')
 );
 
